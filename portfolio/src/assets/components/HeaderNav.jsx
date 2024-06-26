e3942f4a01a2948d073ac43d860d8b9fe515f71d
GuillerFuentec <copelloguillermo60@gmail.com>
2024-06-26 10:01:06 -0400

    Web Portfolio:Fix home in navbar  Beta.1.1 official

--- portfolio/src/assets/components/HeaderNav.jsx
+++ portfolio/src/assets/components/HeaderNav.jsx
@@ -86,10 +86,10 @@ export default function HeaderNav({ toggleTheme }) {
                             <ul>
                               <li>
                                 <a
-                                  href="/about"
-                                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                                  href="/"
+                                  className="block mt-4 px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                                 >
-                                  About Me
+                                  Home
                                 </a>
                               </li>
                               <li>
@@ -152,6 +152,14 @@ export default function HeaderNav({ toggleTheme }) {
 
                     <nav className="pointer-events-auto hidden md:block">
                       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+                        <li>
+                          <a
+                            className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
+                            href="/"
+                          >
+                            Home
+                          </a>
+                        </li>
                         <li>
                           <a
                             className="relative block px-3 py-2 transition hover:text-teal-500 dark:hover:text-teal-400"
